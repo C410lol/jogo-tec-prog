4583197811c5731b76cefcfaecd7d7dc54987297
C410lol <gcaio7463@gmail.com>
2025-11-04 04:00:10 -0300

    adicionando entidades na classe e implementando a colisao entre elas

--- includes/gerenciadores/GerenciadorColisao.h
+++ includes/gerenciadores/GerenciadorColisao.h
@@ -1,7 +1,9 @@
 #ifndef JOGO_COLISION_H
 #define JOGO_COLISION_H
 
+#include "listas/Lista.h"
 #include "entidades/personagens/Jogador.h"
+#include "entidades/personagens/inimigos/Inimigo.h"
 #include "entidades/obstaculos/Obstaculo.h"
 
 
@@ -13,17 +15,36 @@ namespace jogo {
         class GerenciadorColisao
         {
         private:
-            entidades::personagens::Jogador *pJogador;
-            entidades::obstacles::Obstaculo *pObstaculo;
+            listas::Lista<entidades::personagens::Jogador*> *pListaJogadores;
+            listas::Lista<entidades::personagens::inimigos::Inimigo*> *pListaInimigos;
+            listas::Lista<entidades::obstaculos::Obstaculo*> *pListaObstaculos;
+        private:
+            static float calcOverlapHor(entidades::Entidade *ent1, entidades::Entidade *ent2);
+            static float calcOverlapVert(entidades::Entidade *ent1, entidades::Entidade *ent2);
+
+            bool colidiuHor(entidades::Entidade *ent1, entidades::Entidade *ent2);
+            bool colidiuVert(entidades::Entidade *ent1, entidades::Entidade *ent2);
+            bool colidiu(entidades::Entidade *ent1, entidades::Entidade *ent2);
+
+            void checarObstaculo_InimigosColisao(entidades::obstaculos::Obstaculo *pObstaculo);
+            void checarObstaculo_JogadoresColisao(entidades::obstaculos::Obstaculo *pObstaculo);
+
+            void checarInimigo_JogadoresColisao(entidades::personagens::inimigos::Inimigo *pInimigo);
 
         public:
             GerenciadorColisao(
-                entidades::personagens::Jogador *r_pJogador,
-                entidades::obstacles::Obstaculo *r_pObstaculo
+                listas::Lista<entidades::personagens::Jogador*> *r_pListaJogadores,
+                listas::Lista<entidades::personagens::inimigos::Inimigo*> *r_pListaInimigos,
+                listas::Lista<entidades::obstaculos::Obstaculo*> *r_pListaObstaculos
             );
             ~GerenciadorColisao();
 
-            void checarColisao();
+            static sf::Vector2f calcOverlap(entidades::Entidade *ent1, entidades::Entidade *ent2);
+
+            void checarColisoes();
+
+            void checarObstaculoColisoes();
+            void checarInimigoColisoes();
         };
 
     }
--- src/gerenciadores/GerenciadorColisao.cpp
+++ src/gerenciadores/GerenciadorColisao.cpp
@@ -7,24 +7,201 @@ namespace jogo {
     namespace gerenciadores {
 
         GerenciadorColisao::GerenciadorColisao(
-            entidades::personagens::Jogador *r_pJogador,
-            entidades::obstacles::Obstaculo *r_pObstaculo
-        ): pJogador(r_pJogador), pObstaculo(r_pObstaculo)
+            listas::Lista<entidades::personagens::Jogador*> *r_pListaJogadores,
+            listas::Lista<entidades::personagens::inimigos::Inimigo*> *r_pListaInimigos,
+            listas::Lista<entidades::obstaculos::Obstaculo*> *r_pListaObstaculos
+        ):
+        pListaJogadores(r_pListaJogadores),
+        pListaInimigos(r_pListaInimigos),
+        pListaObstaculos(r_pListaObstaculos)
         {}
         GerenciadorColisao::~GerenciadorColisao()
+        {}
+
+
+
+
+        float GerenciadorColisao::calcOverlapHor(
+            entidades::Entidade *ent1,
+            entidades::Entidade *ent2
+        )
         {
-            pJogador = nullptr;
-            pObstaculo = nullptr;
+            if (!(ent1 && ent2))
+                return 0.f;
+
+            float disX = std::abs(
+                            (ent1->getPosicao().x + ent1->getTamanho().x / 2.f) -
+                            (ent2->getPosicao().x + ent2->getTamanho().x / 2.f)
+            );
+            float sumX = (ent1->getTamanho().x / 2.f) + (ent2->getTamanho().x / 2.f);
+
+            if (disX < sumX)
+                return sumX - disX;
+            return 0;
+        }
+        float GerenciadorColisao::calcOverlapVert(
+            entidades::Entidade *ent1,
+            entidades::Entidade *ent2
+        )
+        {
+            float disY = std::abs(
+                            (ent1->getPosicao().y + ent1->getTamanho().y / 2.f) -
+                            (ent2->getPosicao().y + ent2->getTamanho().y / 2.f)
+            );
+            float sumY = (ent1->getTamanho().y / 2.f) + (ent2->getTamanho().y / 2.f);
+
+            if (disY < sumY)
+                return sumY - disY;
+            return 0;
+        }
+        sf::Vector2f GerenciadorColisao::calcOverlap(
+            entidades::Entidade *ent1,
+            entidades::Entidade *ent2
+        )
+        {
+            return sf::Vector2f(calcOverlapHor(ent1, ent2), calcOverlapVert(ent1, ent2));
         }
 
-        void GerenciadorColisao::checarColisao()
+
+
+        bool GerenciadorColisao::colidiuHor(
+            entidades::Entidade *ent1,
+            entidades::Entidade *ent2
+        )
         {
-            if (pObstaculo && pJogador)
+            return calcOverlapHor(ent1, ent2) != 0;
+        }
+        bool GerenciadorColisao::colidiuVert(
+            entidades::Entidade *ent1,
+            entidades::Entidade *ent2
+        )
+        {
+            return calcOverlapVert(ent1, ent2) != 0;
+        }
+        bool GerenciadorColisao::colidiu(
+            entidades::Entidade *ent1,
+            entidades::Entidade *ent2
+        )
+        {
+            return colidiuHor(ent1, ent2) && colidiuVert(ent1, ent2);
+        }
+
+
+
+
+
+
+
+
+        void GerenciadorColisao::checarColisoes()
+        {
+            checarObstaculoColisoes();
+            checarInimigoColisoes();
+        }
+
+
+        void GerenciadorColisao::checarObstaculo_InimigosColisao(
+            entidades::obstaculos::Obstaculo *pObstaculo
+        )
+        {
+            if (pListaInimigos)
             {
-                pObstaculo->obstaculizar(*pJogador);
+                listas::Lista<entidades::personagens::inimigos::Inimigo*>::Iterator itInimigo;
+                for (
+                itInimigo = pListaInimigos->begin();
+                itInimigo != pListaInimigos->end();
+                ++itInimigo
+                )
+                {
+                    if (!(*itInimigo))
+                        continue;
+
+                    if (colidiu(pObstaculo, *itInimigo))
+                        pObstaculo->colidir(*itInimigo);
+                }
+            }
+        }
+        void GerenciadorColisao::checarObstaculo_JogadoresColisao(
+            entidades::obstaculos::Obstaculo *pObstaculo
+        )
+        {
+            if (pListaJogadores)
+            {
+                listas::Lista<entidades::personagens::Jogador*>::Iterator itJogador;
+                for (
+                itJogador = pListaJogadores->begin();
+                itJogador != pListaJogadores->end();
+                ++itJogador
+                )
+                {
+                    if (!(*itJogador))
+                        continue;
+
+                    if (colidiu(pObstaculo, *itJogador))
+                        pObstaculo->colidir(*itJogador);
+                }
+            }
+        }
+        void GerenciadorColisao::checarObstaculoColisoes()
+        {
+            if (!pListaObstaculos)
+                return;
+
+            listas::Lista<entidades::obstaculos::Obstaculo*>::Iterator itObstaculo;
+            for (
+                itObstaculo = pListaObstaculos->begin();
+                itObstaculo != pListaObstaculos->end();
+                ++itObstaculo
+            )
+            {
+                if (!(*itObstaculo))
+                    continue;
+
+                checarObstaculo_InimigosColisao(*itObstaculo);
+                checarObstaculo_JogadoresColisao(*itObstaculo);
             }
         }
 
+
+        void GerenciadorColisao::checarInimigo_JogadoresColisao(
+            entidades::personagens::inimigos::Inimigo *pInimigo
+        )
+        {
+            if (pListaJogadores)
+            {
+                listas::Lista<entidades::personagens::Jogador*>::Iterator itJogador;
+                for (
+                    itJogador = pListaJogadores->begin();
+                    itJogador != pListaJogadores->end();
+                    ++itJogador
+                )
+                {
+                    if (!(*itJogador))
+                        continue;
+
+                    if (colidiu(pInimigo, *itJogador))
+                        pInimigo->colidir(*itJogador);
+                }
+            }
+        }
+        void GerenciadorColisao::checarInimigoColisoes()
+        {
+            if (!pListaInimigos)
+                return;
+
+            listas::Lista<entidades::personagens::inimigos::Inimigo*>::Iterator itInimigo;
+            for (
+                itInimigo = pListaInimigos->begin();
+                itInimigo != pListaInimigos->end();
+                ++itInimigo
+            )
+            {
+                if (!(*itInimigo))
+                    continue;
+
+                checarInimigo_JogadoresColisao(*itInimigo);
+            }
+        }
 
     }
 }

7c6f12f18b047ecb0fef7919481c884f73fc8acb
C410lol <gcaio7463@gmail.com>
2025-11-12 19:20:05 -0300

    comentando atributo inexistente

--- src/entidades/Projetil.cpp
+++ src/entidades/Projetil.cpp
@@ -4,8 +4,8 @@ namespace jogo {
     namespace entidades {
         Projetil::Projetil(personagens::Personagem *r_pPersonagem, float velocidade): pDono(r_pPersonagem) {
             tamanho=sf::Vector2f(5.0f, 5.0f);
-            retangulo.setSize(tamanho);
-            retangulo.setFillColor(sf::Color::White);
+            //retangulo.setSize(tamanho);
+            //retangulo.setFillColor(sf::Color::White);
             sf::Vector2f pos=pDono->getPosicao();
             pos.x+=50;
             setPosicao(pos);
@@ -16,7 +16,7 @@ namespace jogo {
         }
         void Projetil::deslocar() {
             mover(vel, 0.0f);
-            retangulo.setPosition(posicao);
+            //retangulo.setPosition(posicao);
         }
         Projetil::~Projetil() {
 

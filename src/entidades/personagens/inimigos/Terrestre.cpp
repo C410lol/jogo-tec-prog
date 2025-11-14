73598c02fb315ad75e35f95adc4181b19f661245
C410lol <gcaio7463@gmail.com>
2025-11-14 10:49:41 -0300

    atualizando deslocamento

--- src/entidades/personagens/inimigos/Terrestre.cpp
+++ src/entidades/personagens/inimigos/Terrestre.cpp
@@ -40,20 +40,22 @@ namespace jogo {
 
                 void Terrestre::deslocar()
                 {
-                    if (pJogadorAlvo)
-                    {
-                        float disX = pJogadorAlvo->getPosicao().x - getPosicao().x;
+                    if (!pJogadorAlvo)
+                        return;
 
-                        if (disX > 0)
-                        {
-                            olhandoDireita = true;
-                            mover(deslocamento, 0.f);
-                        }
-                        if (disX < 0)
-                        {
-                            olhandoDireita = false;
-                            mover(-deslocamento, 0.f);
-                        }
+                    if (!pJogadorAlvo->getAtivo())
+                        return;
+
+                    float disX = pJogadorAlvo->getPosicao().x - getPosicao().x;
+                    if (disX > 0)
+                    {
+                        olhandoDireita = true;
+                        mover(deslocamento, 0.f);
+                    }
+                    if (disX < 0)
+                    {
+                        olhandoDireita = false;
+                        mover(-deslocamento, 0.f);
                     }
                 }
 

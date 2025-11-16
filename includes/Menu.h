6a11f5b8b22348c0a11ce05bda891ce7456514a0
marcoskowalski <xueyan707@gmail.com>
2025-11-16 20:13:09 -0300

    menu no terminal e cout/cin, irei mudar depois

---
+++ includes/Menu.h
@@ -0,0 +1,24 @@
+
+#ifndef JOGO_MENU_H
+#define JOGO_MENU_H
+#include "Ente.h"
+
+
+
+namespace jogo
+{
+    class Jogo;
+
+    class Menu:public Ente
+    {
+    private:
+        Jogo *pjogo;
+    public:
+        Menu(Jogo *r_pjogo);
+        Menu();
+        ~Menu();
+
+        void executar() override;
+    };
+}
+#endif //JOGO_MENU_H

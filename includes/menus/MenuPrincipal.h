96bd75fcb5a0f82e96011a595cd84ab567f12226
marcoskowalski <xueyan707@gmail.com>
2025-11-19 05:28:54 -0300

    Menu principal, tem seu metodo desenhar e eh executado pelo executar do EstadoMenu, texture do menu png e interativo somente com o mouse por enquanto;

---
+++ includes/menus/MenuPrincipal.h
@@ -0,0 +1,28 @@
+//
+// Created by mvmko on 18/11/2025.
+//
+
+#ifndef JOGO_MENUPRINCIPAL_H
+#define JOGO_MENUPRINCIPAL_H
+#include "Menu.h"
+
+
+#include "Menu.h"
+#include "Botao.h"
+
+namespace jogo {
+    namespace menus {
+
+        class MenuPrincipal : public Menu {
+        public:
+            MenuPrincipal();
+            ~MenuPrincipal();
+
+            void atualizar() override;
+            void desenhaMenu();
+            void executar();
+        };
+
+    }
+}
+#endif //JOGO_MENUPRINCIPAL_H

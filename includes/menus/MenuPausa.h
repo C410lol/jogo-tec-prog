281ec4ee8105c64b666652c13ba03d08a1b3dd8d
marcoskowalski <xueyan707@gmail.com>
2025-11-19 05:31:17 -0300

    MenuPausa, tem seu metodo desenhar e eh executado pelo executar do EstadoMenu, o fundo n tem textura e ainda nao esta completo pois ainda preciso fazer desenhar a fase, que no momento eh um nullptr

---
+++ includes/menus/MenuPausa.h
@@ -0,0 +1,27 @@
+//
+// Created by mvmko on 19/11/2025.
+//
+
+#ifndef JOGO_MENUPAUSA_H
+#define JOGO_MENUPAUSA_H
+#include "Menu.h"
+#include "fases/Fase.h"
+
+namespace jogo {
+    namespace menus {
+        class MenuPausa : public Menu {
+        private:
+            fases::Fase* fase;
+            sf::RectangleShape fundoPainel;
+        public:
+            void atualizar();
+            MenuPausa(fases::Fase* fase = nullptr);
+            void setFase(fases::Fase* fase);
+
+            fases::Fase* getFase();
+            virtual void executar();
+            void desenhaMenu() override;
+        };
+    }
+}
+#endif //JOGO_MENUPAUSA_H
---
+++ src/menus/MenuPausa.cpp
@@ -0,0 +1,53 @@
+
+
+
+#include "menus/MenuPausa.h"
+
+#include <iostream>
+
+#include "gerenciadores/GerenciadorEstado.h"
+#include "gerenciadores/GerenciadorGrafico.h"
+
+namespace jogo {
+    namespace menus {
+        MenuPausa::MenuPausa(fases::Fase* r_fase) : fundoPainel(), fase(r_fase){
+            botoes.push_back(new Botao("Sair",{500.f, 175.f}, {200.f, 50.f}));
+            botoes.push_back(new Botao("Continuar",{500.f, 300.f}, {200.f, 50.f}));
+
+            fundoPainel.setSize(sf::Vector2f(400.f, 300.f));   // largura e altura
+            fundoPainel.setFillColor(sf::Color(0, 0, 0, 150)); // preto semi-transparente
+            fundoPainel.setPosition(400.f, 200.f);
+        }
+        void MenuPausa::atualizar() {
+            sf::Vector2f mousePos = sf::Vector2f(sf::Mouse::getPosition(*pGerenciadorGrafico->getJanela()));
+
+            if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+                if (botoes[0]->contem(mousePos)) {
+                    pge->limparEstados();                     // esvazia toda a pilha
+                    pge->adicionarEstado(IDs::menu_principal);
+
+                }
+                if (botoes[1]->contem(mousePos)) {
+                    pge->removerEstado();
+                    pge->getEstadoAtual()->setvoltaMenu(false);
+                }
+            }
+        }
+        void MenuPausa::executar() {
+            if (fase != nullptr) {
+                fase->desenhar();
+            }
+            desenhaMenu();
+            atualizar();
+        }
+        void MenuPausa::desenhaMenu() {
+            pGerenciadorGrafico->desenhar(fundoPainel);
+            for (size_t i = 0; i < botoes.size(); ++i) {
+                botoes[i]->desenhar();
+            }
+        }
+
+
+        }
+
+    }

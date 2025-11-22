2ddf92a1896afef0ffded347e14fb12e43952640
marcoskowalski <xueyan707@gmail.com>
2025-11-22 18:26:23 -0300

    bandeira

---
+++ src/entidades/obstaculos/Bandeira.cpp
@@ -0,0 +1,37 @@
+#include "entidades/obstaculos/Bandeira.h"
+
+#include "entidades/personagens/Jogador.h"
+#include "fases/Fase.h"
+
+
+namespace jogo {
+    namespace entidades {
+        namespace obstaculos {
+            Bandeira::Bandeira(sf::Vector2f r_posicao, sf::Vector2f r_tamanho, bool r_danoso) :
+            Obstaculo(r_posicao, r_tamanho, r_danoso),
+            pge(gerenciadores::GerenciadorEstado::getGerenEstado()){
+
+                setTexture("../assets/obstaculos/Waving Flag Red.gif");
+                fixTexture();
+
+            }
+            Bandeira::~Bandeira() {
+
+            }
+            void Bandeira::obstaculizar(Entidade *pEntidade) {
+                personagens::Jogador* pJogador = dynamic_cast<personagens::Jogador*>(pEntidade);
+                if (!pJogador)
+                    return;
+                pge->adicionarEstado(IDs::segunda_fase, fases::Fase::pjogo->getTam() + 1);
+
+            }
+            void Bandeira::executar() {
+
+            }
+
+
+
+
+        }
+    }
+}
